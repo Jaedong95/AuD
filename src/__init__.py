b22c70f89a1ebb55f27eb3023a8dd23c1bf37a89
Jaedong95 <badzoo@naver.com>
2023-03-10 17:44:49 +0900

    .

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from .processor import BertDataset, BertProcessor
+from .processor import BertDataset, BertProcessor, AuDWord2Vec, DSMProcessor
 from .trainer import BertRegressor, BertRegTrainer, BertClsTrainer
 from .tester import BertRegTester, BertClsTester
 from .db_connector import AuDDB
